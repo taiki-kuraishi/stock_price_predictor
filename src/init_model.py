aa9691125a6c4197521a5dbf4be5e2fd5c08884e
taiki-kuraishi <20122027@kaishi-pu.ac.jp>
2023-12-11 18:53:43 +0900

    Update init_model.py

--- src/init_model.py
+++ src/init_model.py
@@ -2,48 +2,47 @@ import os
 import joblib
 import pandas as pd
 from dotenv import load_dotenv
+from modules.shift_data_for_prediction import shift_dataFrame
+from modules.preprocessing_for_prediction import split_target_and_features
 from sklearn.linear_model import PassiveAggressiveRegressor
+
+PREDICTION_HORIZON = 1
 
 load_dotenv()
 stock_name = os.environ["STOCK_NAME"]
 
 # read preprocessed csv
 try:
-    df = pd.read_csv("../data/" + stock_name + "_1h_preprocessed.csv", encoding="utf-8")
+    df = pd.read_csv(
+        "../data/" + stock_name + "_1h_preprocessed.csv",
+        encoding="utf-8",
+        index_col=0,
+    )
     print("success read csv")
 except Exception as e:
     print("on error read csv")
     print(e)
     exit()
 
+# create shifted data for prediction
+df = shift_dataFrame(df, PREDICTION_HORIZON)
+
 # set target and explanatory variables
-try:
-    drop_columns = ["Datetime", "Close"]
-    x = df.drop(drop_columns, axis=1)
-    y = df["Close"]
-    print("success set target and explanatory variables")
-except Exception as e:
-    print("on error set target and explanatory variables")
-    print(e)
-    exit()
+x, y = split_target_and_features(df)
 
 # model construction
-try:
-    model = PassiveAggressiveRegressor()
-    model.fit(x, y)
-    print("success model construction")
-except Exception as e:
-    print("on error model construction")
-    print(e)
-    exit()
+model = PassiveAggressiveRegressor()
+model.fit(x, y)
 
 # output model
-try:
-    joblib.dump(model, "../models/" + stock_name + "_1h_PassiveAggressiveRegressor.pkl")
-    print("success output model")
-except Exception as e:
-    print("on error output model")
-    print(e)
-    exit()
+joblib.dump(
+    model,
+    "../models/"
+    + stock_name
+    + "_"
+    + str(PREDICTION_HORIZON)
+    + "h_PassiveAggressiveRegressor.pkl",
+)
+print("success output model")
 
 print("finish")
