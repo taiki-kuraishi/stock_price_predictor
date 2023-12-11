68294403a3432f4b6c5ceb335743466af68a66b9
taiki-kuraishi <20122027@kaishi-pu.ac.jp>
2023-12-11 18:53:41 +0900

    Update init_preprocessed_csv.py

--- src/init_preprocessed_csv.py
+++ src/init_preprocessed_csv.py
@@ -1,7 +1,7 @@
 import os
-import pandas as pd
 import yfinance as yf
 from dotenv import load_dotenv
+from modules.preprocessing_for_prediction import split_datetime
 
 load_dotenv()
 target_stock = os.environ["TARGET_STOCK"]
@@ -16,47 +16,9 @@ except Exception as e:
     print("on error yfinance")
 
 # format datetime
-try:
-    df = df.reset_index()
-    df["Datetime"] = pd.to_datetime(df["Datetime"], utc=True)
-    df["year"] = df["Datetime"].dt.year
-    df["month"] = df["Datetime"].dt.month
-    df["day"] = df["Datetime"].dt.day
-    df["hour"] = df["Datetime"].dt.hour
-    df["dayofweek"] = df["Datetime"].dt.dayofweek
-
-    df = df[
-        [
-            "Datetime",
-            "year",
-            "month",
-            "day",
-            "hour",
-            "dayofweek",
-            "Open",
-            "High",
-            "Low",
-            "Close",
-            "Adj Close",
-            "Volume",
-        ]
-    ]
-
-    df = df.sort_values("Datetime")
-
-    df = df.reset_index(drop=True)
-
-    print("success format data")
-except Exception as e:
-    print("on error format data")
-    print(e)
-    exit()
+df = split_datetime(df)
 
 # save as csv
-try:
-    df.to_csv("../data/" + stock_name + "_1h_preprocessed.csv")
-    print("success save as csv")
-except Exception as e:
-    print("on error save as csv")
-    print(e)
-    exit()
+df.to_csv("../data/" + stock_name + "_1h_preprocessed.csv", encoding="utf-8")
+
+print("finish")
