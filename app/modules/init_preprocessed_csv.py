99477b665bc6e275ec087d843216e4684150806e
taiki-kuraishi <131664944+taiki-kuraishi@users.noreply.github.com>
2023-12-15 12:58:34 +0900

    Create init_preprocessed_csv.py

---
+++ app/modules/init_preprocessed_csv.py
@@ -0,0 +1,40 @@
+import yfinance as yf
+from .preprocessing_for_prediction import split_datetime
+
+
+def init_preprocessed_csv(
+    csv_dir: str,
+    stock_name: str,
+    target_stock: str,
+    period: str,
+    interval: str,
+    predict_horizon: int,
+) -> None:
+    # get data from yahoo finance
+    try:
+        df = yf.download(tickers=target_stock, period=period, interval=interval)
+        print(df)
+        if df.empty:
+            print("fail to get data from yahoo finance")
+            return
+        else:
+            print("success to get data from yahoo finance")
+    except Exception as e:
+        print(e)
+        print("fail to get data from yahoo finance")
+        return
+
+    # reset index and rename column
+    df.reset_index(inplace=True)
+    df.rename(columns={"Date": "Datetime"}, inplace=True)
+
+    # format datetime
+    splitted_datetime_df = split_datetime(df)
+
+    # save as csv
+    splitted_datetime_df.to_csv(
+        f"{csv_dir}/{stock_name}_{str(predict_horizon)}h_preprocessed.csv",
+        encoding="utf-8",
+    )
+
+    print("init_preprocessed_csv finish")
