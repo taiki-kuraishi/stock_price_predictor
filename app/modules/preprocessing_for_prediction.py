ae36858f7c02cc50281068e211167dd793cf6885
taiki-kuraishi <131664944+taiki-kuraishi@users.noreply.github.com>
2023-12-15 12:58:37 +0900

    Create preprocessing_for_prediction.py

---
+++ app/modules/preprocessing_for_prediction.py
@@ -0,0 +1,43 @@
+import pandas as pd
+
+
+def split_datetime(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    split datetime to year, month, day, hour, dayofweek
+    """
+
+    df.loc[:, "Datetime"] = pd.to_datetime(df["Datetime"], utc=True)
+    df.loc[:, "year"] = df["Datetime"].dt.year
+    df.loc[:, "month"] = df["Datetime"].dt.month
+    df.loc[:, "day"] = df["Datetime"].dt.day
+    df.loc[:, "hour"] = df["Datetime"].dt.hour
+    df.loc[:, "dayofweek"] = df["Datetime"].dt.dayofweek
+
+    df = df[
+        [
+            "Datetime",
+            "year",
+            "month",
+            "day",
+            "hour",
+            "dayofweek",
+            "Open",
+            "High",
+            "Low",
+            "Close",
+            "Adj Close",
+            "Volume",
+        ]
+    ]
+
+    return df
+
+
+def split_target_and_features(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    split target and features
+    """
+    drop_columns = ["Datetime", "Close"]
+    y = df["Close"]
+    x = df.drop(drop_columns, axis=1)
+    return x, y
