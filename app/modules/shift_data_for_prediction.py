d5be61647908abc0b75eb6cf7aae88943a4dea21
taiki-kuraishi <131664944+taiki-kuraishi@users.noreply.github.com>
2023-12-16 12:29:18 +0900

    Update shift_data_for_prediction.py

--- app/modules/shift_data_for_prediction.py
+++ app/modules/shift_data_for_prediction.py
@@ -2,28 +2,12 @@ import pandas as pd
 
 
 def shift_dataFrame(df: pd.DataFrame, shift_rows: int) -> pd.DataFrame:
+    """
+    shift dataFrame
+    shift_rows時間後の終値を予測するために、shift_rows行分だけデータをずらす
+    """
     df_shifted = df.copy()
     df_shifted["Close"] = df_shifted["Close"].shift(-shift_rows)
     df_shifted.dropna(inplace=True, subset=["Close"])
 
     return df_shifted
-
-
-if __name__ == "__main__":
-    import os
-    from dotenv import load_dotenv
-
-    load_dotenv(dotenv_path="../.env")
-    stock_name = os.environ["STOCK_NAME"]
-
-    df = pd.read_csv(
-        "../../data/" + stock_name + "_1h_preprocessed.csv",
-        encoding="utf-8",
-        index_col=0,
-    )
-
-    print(df.tail())
-
-    df_shifted = shift_dataFrame(df, 1)
-
-    print(df_shifted.tail())
