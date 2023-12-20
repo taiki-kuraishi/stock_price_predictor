bac810f64debfe403791c7b52a83c4993370eac9
taiki-kuraishi <20122027@kaishi-pu.ac.jp>
2023-12-20 19:32:51 +0900

    Add: Test execution block

--- app/modules/model_operations.py
+++ app/modules/model_operations.py
@@ -1,9 +1,9 @@
-# import joblib
 import pandas as pd
 from sklearn.linear_model import PassiveAggressiveRegressor
-from .preprocessing_for_prediction import split_target_and_features, shift_dataFrame
+from preprocessing_for_prediction import shift_dataFrame, split_target_and_features
 
 
+# モデルの学習
 def train_model(df: pd.DataFrame, predict_horizon: int) -> PassiveAggressiveRegressor:
     """
     init model
@@ -19,19 +19,14 @@ def train_model(df: pd.DataFrame, predict_horizon: int) -> PassiveAggressiveRegr
     model = PassiveAggressiveRegressor()
     model.fit(x, y)
 
-    # # output model
-    # joblib.dump(
-    #     model,
-    #     f"{tmp_dir}/{stock_name}_{str(predict_horizon)}h_PassiveAggressiveRegressor.pkl",
-    # )
-
-    print("init_model finish")
+    print("train model complete")
     return model
 
 
+# 予測
 def make_predictions(
-    model: PassiveAggressiveRegressor, df: pd.DataFrame, predict_horizon: int
-):
+    model: PassiveAggressiveRegressor, df: pd.DataFrame
+) -> list[float]:
     """
     predict
     modelによる予測を行う
@@ -41,10 +36,57 @@ def make_predictions(
 
     # predict
     y_predict = model.predict(x)
-    print("predict: " + str(y_predict))
 
     return y_predict
 
 
-def init_dynamodb():
-    print("init_dynamodb finish")
+if __name__ == "__main__":
+    import os
+    from dotenv import load_dotenv
+    from dynamodb_fetcher import get_data_from_dynamodb, upload_data_to_dynamodb
+
+    load_dotenv(dotenv_path="../../.env", override=True)
+    df_col_order: list = os.getenv("DTAFRAME_COLUMNS_ORDER").split(",")
+    predict_horizon: int = 1
+    aws_region_name: str = os.getenv("AWS_REGION_NAME")
+    aws_access_key_id: str = os.getenv("AWS_ACCESS_KEY_ID")
+    aws_secret_access_key: str = os.getenv("AWS_SECRET_ACCESS_KEY")
+    aws_dynamodb_train_table_name: str = os.getenv("AWS_DYNAMODB_TRAIN_TABLE_NAME")
+    aws_dynamo_prediction_table_name: str = os.getenv(
+        "AWS_DYNAMODB_PREDICTION_TABLE_NAME"
+    )
+
+    # get train data
+    df = get_data_from_dynamodb(
+        df_col_order,
+        aws_region_name,
+        aws_access_key_id,
+        aws_secret_access_key,
+        aws_dynamodb_train_table_name,
+    )
+
+    # train model
+    model = train_model(df, predict_horizon)
+
+    # predict
+    y_predict = make_predictions(model, df)
+
+    # 新しいdfの作成
+    df_predict = pd.DataFrame()
+    df_predict["datetime"] = df[["datetime"]]
+
+    # concat predict to df_predict
+    df_predict[f"{predict_horizon}_pred"] = y_predict
+
+    print(df_predict)
+
+    # # upload to dynamodb
+    # upload_data_to_dynamodb(
+    #     aws_region_name,
+    #     aws_access_key_id,
+    #     aws_secret_access_key,
+    #     aws_dynamo_prediction_table_name,
+    #     df_predict,
+    # )
+
+    print("model operation complete")
