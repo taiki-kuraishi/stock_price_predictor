06d859c730a60f1e741b98275227b8a6f60b31f5
Kuraishi Taiki <20122027@kaishi-pu.ac.jp>
2024-01-02 15:31:00 +0900

    Update dynamodb_fetcher.py

--- app/modules/dynamodb_fetcher.py
+++ app/modules/dynamodb_fetcher.py
@@ -1,6 +1,7 @@
 import boto3
 import pandas as pd
 
+
 def get_data_from_dynamodb(
     region_name: str,
     access_key_id: str,
@@ -11,26 +12,17 @@ def get_data_from_dynamodb(
     get data from dynamodb
     dynamodbからデータを取得する
     """
-    try:
-        dynamodb = boto3.resource(
-            "dynamodb",
-            region_name=region_name,
-            aws_access_key_id=access_key_id,
-            aws_secret_access_key=secret_access_key,
-        )
+    dynamodb = boto3.resource(
+        "dynamodb",
+        region_name=region_name,
+        aws_access_key_id=access_key_id,
+        aws_secret_access_key=secret_access_key,
+    )
 
-        table = dynamodb.Table(dynamodb_table_name)
+    table = dynamodb.Table(dynamodb_table_name)
 
-        try:
-            response = table.scan()
-        except Exception as e:
-            print(e)
-            raise Exception("fail to get data from dynamodb")
+    response = table.scan()
 
-        df = pd.DataFrame(response["Items"])
-    except Exception as e:
-        print(e)
-        raise Exception("fail to function on get_data_from_dynamodb")
+    df = pd.DataFrame(response["Items"])
 
     return df
-
