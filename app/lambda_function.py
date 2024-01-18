d955694ba2d12ac07733875e5137b769c2029911
Kuraishi Taiki <20122027@kaishi-pu.ac.jp>
2024-01-18 16:07:37 +0900

    Refactor: Change environment variable names to uppercase for consistency

--- app/lambda_function.py
+++ app/lambda_function.py
@@ -9,38 +9,43 @@ from aws_lambda_context import LambdaContext
 
 def lambda_handler(event: dict, context: LambdaContext):
     # load env
-    timezone: str = os.environ["TIMEZONE"]
-    stock_name: str = os.environ["STOCK_NAME"]
-    region_name: str = os.environ["REGION_NAME"]
-    access_key_id: str = os.environ["ACCESS_KEY_ID"]
-    secret_access_key: str = os.environ["SECRET_ACCESS_KEY"]
-    time_list: list[int] = [int(i) for i in os.environ["TIME_LIST"].split(",")]
-    dict_order: list[str] = os.environ["DICT_ORDER"].split(",")
-    dynamodb_pred_table_name = "stock_price_predictor_" + stock_name + "_prediction"
+    SATURDAY: int = os.environ["SATURDAY"]
+    SUNDAY: int = os.environ["SUNDAY"]
+    TIMEZONE: str = os.environ["TIMEZONE"]
+    STOCK_NAME: str = os.environ["STOCK_NAME"]
+    REGION_NAME: str = os.environ["REGION_NAME"]
+    ACCESS_KEY_ID: str = os.environ["ACCESS_KEY_ID"]
+    SECRET_ACCESS_KEY: str = os.environ["SECRET_ACCESS_KEY"]
+    TIME_LIST: list[int] = [int(i) for i in os.environ["TIME_LIST"].split(",")]
+    DICT_ORDER: list[str] = os.environ["DICT_ORDER"].split(",")
+    DYNAMODB_PRED_TABLE_NAME: str = (
+        "stock_price_predictor_" + STOCK_NAME + "_prediction"
+    )
 
     # set timezone
-    JST = pytz.timezone(timezone)
+    JST = pytz.timezone(TIMEZONE)
 
     # aws instance
     dynamodb = boto3.resource(
         "dynamodb",
-        region_name=region_name,
-        aws_access_key_id=access_key_id,
-        aws_secret_access_key=secret_access_key,
+        region_name=REGION_NAME,
+        aws_access_key_id=ACCESS_KEY_ID,
+        aws_secret_access_key=SECRET_ACCESS_KEY,
     )
-    table = dynamodb.Table(dynamodb_pred_table_name)
+    table = dynamodb.Table(DYNAMODB_PRED_TABLE_NAME)
 
     if "handler" not in event:
         return {
             "message": "bad request",
         }
 
-    # hello world
     if event["handler"] == "latest":
         todays_date = datetime.now(JST).strftime("%Y-%m-%d")
+
         response = table.query(KeyConditionExpression=Key("date").eq(todays_date))[
             "Items"
         ]
+
         if len(response) != 0:
             # datetimeでソート
             response_df = pd.DataFrame(response)
@@ -59,7 +64,7 @@ def lambda_handler(event: dict, context: LambdaContext):
             prediction_dict = response_df.tail(1).to_dict("records")[0]
 
         # sort dict key
-        prediction_dict = {key: prediction_dict[key] for key in dict_order}
+        prediction_dict = {key: prediction_dict[key] for key in DICT_ORDER}
 
         response_body = {
             "prediction_timestamp": prediction_dict["create_at"],
@@ -69,47 +74,49 @@ def lambda_handler(event: dict, context: LambdaContext):
         # predictionBaseTimeからhourを取得
         predictionBaseHour = prediction_dict["datetime"].hour
 
-        # creation_datetimeの時間がtime_listにない場合はエラー
-        if predictionBaseHour not in time_list:
-            print("time_list : ", time_list)
-            print("predictionBaseHour : ", predictionBaseHour)
-            raise Exception("predictionBaseHour not in time_list")
+        # creation_datetimeの時間がtime_listにない場合は、最も近い時間を取得
+        if predictionBaseHour not in TIME_LIST:
+            # time_listの中で最も近い時間を取得
+            predictionBaseHour = min(
+                TIME_LIST, key=lambda x: abs(x - predictionBaseHour)
+            )
 
-        time_list_index = time_list.index(predictionBaseHour)
+        time_list_index = TIME_LIST.index(predictionBaseHour)
+
+        # pred_time
+        pred_time = prediction_dict["datetime"]
+        print("pred_time : ", pred_time)
+
+        # drop date,time,datetime, create_at
+        prediction_dict.pop("date")
+        prediction_dict.pop("time")
+        prediction_dict.pop("datetime")
+        prediction_dict.pop("create_at")
 
         # create prediction dict
         for key, value in prediction_dict.items():
-            if (
-                key == "date"
-                or key == "time"
-                or key == "datetime"
-                or key == "create_at"
-            ):
-                continue
+            datetime_value = pred_time
 
-            index = int(key.split("_")[0])
-            pred_time = prediction_dict["datetime"]
-
-            if time_list_index + index >= len(time_list):
-                pred_time = pred_time.replace(day=pred_time.day + 1)
+            if time_list_index + int(key) >= len(TIME_LIST):
+                datetime_value = datetime_value.replace(day=datetime_value.day + 1)
 
                 # weakDayが5,6(土、日)の場合は月曜日まで日付を進める
-                if pred_time.weekday() == 5:
-                    pred_time = pred_time.replace(day=pred_time.day + 2)
-                elif pred_time.weekday() == 6:
-                    pred_time = pred_time.replace(day=pred_time.day + 1)
+                if datetime_value.weekday() == SATURDAY:
+                    datetime_value = datetime_value.replace(day=datetime_value.day + 2)
+                elif datetime_value.weekday() == SUNDAY:
+                    datetime_value = datetime_value.replace(day=datetime_value.day + 1)
 
-                pred_time = pred_time.replace(
-                    hour=int(time_list[time_list_index + index - len(time_list)])
+                datetime_value = datetime_value.replace(
+                    hour=int(TIME_LIST[time_list_index + int(key) - len(TIME_LIST)])
                 )
             else:
-                pred_time = pred_time.replace(
-                    hour=int(time_list[time_list_index + index])
+                datetime_value = datetime_value.replace(
+                    hour=int(TIME_LIST[time_list_index + int(key)])
                 )
-            pred_time = pred_time.isoformat()
-            response_body["prediction"][str(index) + "_hour_prediction"] = {
+
+            response_body["prediction"][str(int(key)) + "_hour_prediction"] = {
                 "value": str(value),
-                "datetime": pred_time,
+                "datetime": datetime_value.isoformat(),
             }
 
         print("response_body : ", response_body)
